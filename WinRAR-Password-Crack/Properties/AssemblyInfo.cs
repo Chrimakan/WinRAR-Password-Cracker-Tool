006e771b5e63dda9d16b3f539772973da5367729
Denis T. <deadspell21391@gmail.com>
2025-10-20 11:32:38 +0200

    AssemblyInfo.cs

---
+++ WinRAR-Password-Crack/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinRAR Password Cracker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinRAR Password Cracker")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9FACD3E1-263C-4D5B-9475-AFDFDFF8C6CF")]
+
+[assembly: AssemblyVersion("4.97.88.2536")]
+[assembly: AssemblyFileVersion("4.97.88.2536")]
